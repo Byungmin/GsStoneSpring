7ad4c83148c88525643125951ab1d3405800ec53
byungmin <it_wangchun@naver.com>
2016-03-04 00:10:55 +0900

    testController

---
+++ GsStoneSpring/src/main/java/gs/test/controller/TestController.java
@@ -0,0 +1,19 @@
+package gs.test.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	Logger log = Logger.getLogger(this.getClass());
+	
+	
+	@RequestMapping(value="/testcontroller.gs")
+	public String testController() throws Exception{
+		log.debug("intercepter Test ");
+		return "test";
+	}
+	
+}
